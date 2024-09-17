45e83325cd8876b9e9aa85437b318a493b3de9a6
smmazharul <hmoxygen@gmail.com>
2024-09-17 17:49:39 +0600

    initial comment

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
+import cors from 'cors'
+import products from '../backend/data/products.js'
+// const express = require(express)
+const port = process.env.PORT || 5000;
+
+connectDB() // connect to mongodb 
+
+const app = express();
+
+
+app.use(cors());
+app.get('/', (req,res) => {
+    res.send("API is running ...");
+
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((product)=>product._id === req.params.id);
+    res.json(product)
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
