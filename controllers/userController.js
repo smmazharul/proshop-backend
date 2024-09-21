e1e46ac3b40e9fd6e5b5114066f1af388b4af1e6
smmazharul <hmoxygen@gmail.com>
2024-09-21 13:22:54 +0600

    set the token and add cookie also install cookie-parser

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,13 +11,22 @@ const authUser = asyncHandler(async (req, res) => {
    const {email, password} = req.body;
    const user = await User.findOne({email});
    if(user && (await user.matchPassword(password))){
-     const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: "30d"})
+     const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: "30d"});
+
+     //set jwt as httpOnly cookie
+     res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "strict",
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+     });
+     
       res.json({
          _id: user._id,
          name: user.name,
          email: user.email,
          isAdmin: user.isAdmin,
-         token
+         
       })
    }else{
       res.status(401)
